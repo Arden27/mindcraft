c3dde16784e731328e653c642d9532701a77fd6d
Artem Furman <artemfurman@gmail.com>
2023-12-12 21:55:10 +0100

    customize the PointerLockControls to anable the revert camera

--- src/components/ThreeCanvas.tsx
+++ src/components/ThreeCanvas.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
-import { PointerLockControls } from "three/addons/controls/PointerLockControls.js";
+//import { PointerLockControls } from "three/addons/controls/PointerLockControls.js";
+import { CustomPointerLockControls } from "@/components/CustomPointerLockControls";
 
 let WORLD_SIZE = 10;
 
@@ -29,6 +30,7 @@ const ThreeCanvas = () => {
     up: false,
     down: false,
   }).current; // Use ref to ensure synchronous updates
+  const [cameraPosition, setCameraPosition] = useState({ x: 0, y: 0, z: 0 });
 
   useEffect(() => {
     const world = new World();
@@ -44,7 +46,10 @@ const ThreeCanvas = () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     canvasRef.current?.appendChild(renderer.domElement);
 
-    const controls = new PointerLockControls(camera, document.body);
+    const controls = new CustomPointerLockControls(camera, document.body);
+    // controls.minPolarAngle = 0; // Minimum vertical rotation (0 = directly downwards)
+    // controls.maxPolarAngle = Math.PI; // Maximum vertical rotation (Math.PI = directly upwards)
+
     canvasRef.current?.addEventListener("click", () => {
       controls.lock();
     });
@@ -100,51 +105,72 @@ const ThreeCanvas = () => {
 
     const animate = () => {
       requestAnimationFrame(animate);
-    
+
       if (controls.isLocked) {
         const speed = 0.1; // Movement speed
         const verticalSpeed = 1.0; // Vertical movement speed; adjust as needed
         const forwardDirection = new THREE.Vector3();
         const rightDirection = new THREE.Vector3();
         const upDirection = camera.position.clone().normalize(); // Direction from the camera towards the center of the planet
-    
+
         // Get the forward direction relative to the camera's current rotation
         camera.getWorldDirection(forwardDirection);
         forwardDirection.normalize();
         rightDirection.crossVectors(camera.up, forwardDirection).normalize();
-    
+
         // Project the movement directions onto the tangent plane at the camera's position
-        const tangentForward = forwardDirection.sub(upDirection.clone().multiplyScalar(forwardDirection.dot(upDirection)));
-        const tangentRight = rightDirection.sub(upDirection.clone().multiplyScalar(rightDirection.dot(upDirection)));
-    
+        const tangentForward = forwardDirection.sub(
+          upDirection.clone().multiplyScalar(forwardDirection.dot(upDirection)),
+        );
+        const tangentRight = rightDirection.sub(
+          upDirection.clone().multiplyScalar(rightDirection.dot(upDirection)),
+        );
+
         if (moveState.forward) {
           camera.position.addScaledVector(tangentForward, speed);
         }
         if (moveState.backward) {
           camera.position.addScaledVector(tangentForward, -speed);
         }
+        // Determine if the camera is below the equator
+        const isBelowEquator = camera.position.y < 0;
+
+        // Adjust the movement direction based on the camera's position
+        const lateralMovementFactor = isBelowEquator ? -1 : 1;
+
         if (moveState.left) {
-          camera.position.addScaledVector(tangentRight, speed);
+          camera.position.addScaledVector(
+            tangentRight,
+            lateralMovementFactor * speed,
+          );
         }
         if (moveState.right) {
-          camera.position.addScaledVector(tangentRight, -speed);
+          camera.position.addScaledVector(
+            tangentRight,
+            lateralMovementFactor * -speed,
+          );
         }
-    
+
         if (moveState.up) {
           camera.position.setLength(camera.position.length() + verticalSpeed);
         } else if (moveState.down) {
           camera.position.setLength(camera.position.length() - verticalSpeed);
         }
-    
+
         // Prevent the camera from going below the surface
         if (camera.position.length() < WORLD_SIZE + 1) {
           camera.position.setLength(WORLD_SIZE + 1);
         }
       }
-    
+
+      setCameraPosition({
+        x: camera.position.x,
+        y: camera.position.y,
+        z: camera.position.z,
+      });
+
       renderer.render(world, camera);
     };
-    
 
     animate();
 
@@ -166,7 +192,16 @@ const ThreeCanvas = () => {
     };
   }, []);
 
-  return <div ref={canvasRef}></div>;
+  return (
+    <>
+      <div className="absolute left-0 top-0 text-white">
+        {`Camera Position: x=${cameraPosition.x.toFixed(
+          2,
+        )}, y=${cameraPosition.y.toFixed(2)}, z=${cameraPosition.z.toFixed(2)}`}
+      </div>
+      <div ref={canvasRef}></div>
+    </>
+  );
 };
 
 export default ThreeCanvas;
