cd7c0e26ba702c321c631ec1dede2c33365c28ef
Artem Furman <artemfurman@gmail.com>
2023-12-12 18:48:30 +0100

    test camra-controls lib

--- src/components/ThreeCanvas.tsx
+++ src/components/ThreeCanvas.tsx
@@ -2,14 +2,14 @@
 
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { PointerLockControls } from "three/addons/controls/PointerLockControls.js";
+import CameraControls from 'camera-controls';
 
 let WORLD_SIZE = 10;
 
 class World extends THREE.Scene {
   constructor() {
     super();
-    const worldGeometry = new THREE.SphereGeometry(WORLD_SIZE, 64, 64); // large radius for the sphere
+    const worldGeometry = new THREE.SphereGeometry(WORLD_SIZE, 64, 64);
     const worldMaterial = new THREE.MeshBasicMaterial({
       color: 0xaaaaaa,
       wireframe: true,
@@ -21,14 +21,6 @@ class World extends THREE.Scene {
 
 const ThreeCanvas = () => {
   const canvasRef = useRef<HTMLDivElement>(null);
-  const moveState = useRef({
-    forward: false,
-    backward: false,
-    left: false,
-    right: false,
-    up: false,
-    down: false,
-  }).current; // Use ref to ensure synchronous updates
 
   useEffect(() => {
     const world = new World();
@@ -38,113 +30,45 @@ const ThreeCanvas = () => {
       0.1,
       1000,
     );
-    camera.position.set(0, WORLD_SIZE + 1, 0);
+    camera.position.set(0, WORLD_SIZE + 10, 0);
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     canvasRef.current?.appendChild(renderer.domElement);
 
-    const controls = new PointerLockControls(camera, document.body);
-    canvasRef.current?.addEventListener("click", () => {
-      controls.lock();
+    CameraControls.install({ THREE: THREE });
+    const controls = new CameraControls(camera, renderer.domElement);
+
+    canvasRef.current?.addEventListener('click', () => {
+      canvasRef.current?.requestPointerLock();
     });
 
-    const onKeyDown = (event: KeyboardEvent) => {
-      switch (event.code) {
-        case "KeyW":
-          moveState.forward = true;
-          break;
-        case "KeyS":
-          moveState.backward = true;
-          break;
-        case "KeyA":
-          moveState.left = true;
-          break;
-        case "KeyD":
-          moveState.right = true;
-          break;
-        case "Space":
-          moveState.up = true;
-          break;
-        case "ControlLeft": // Use "ControlLeft" for left control key
-          moveState.down = true;
-          break;
+    const handleMouseMove = (event: any) => {
+      const movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+      const movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+      const rotationSpeed = 0.005;
+      controls.rotate(-movementX * rotationSpeed, -movementY * rotationSpeed, false);
+    };
+
+    const handlePointerLockChange = () => {
+      if (document.pointerLockElement === canvasRef.current) {
+        document.addEventListener('mousemove', handleMouseMove);
+      } else {
+        document.removeEventListener('mousemove', handleMouseMove);
       }
     };
 
-    const onKeyUp = (event: KeyboardEvent) => {
-      switch (event.code) {
-        case "KeyW":
-          moveState.forward = false;
-          break;
-        case "KeyS":
-          moveState.backward = false;
-          break;
-        case "KeyA":
-          moveState.left = false;
-          break;
-        case "KeyD":
-          moveState.right = false;
-          break;
-        case "Space":
-          moveState.up = false;
-          break;
-        case "ControlLeft":
-          moveState.down = false;
-          break;
-      }
-    };
+    document.addEventListener('pointerlockchange', handlePointerLockChange);
 
-    document.addEventListener("keydown", onKeyDown);
-    document.addEventListener("keyup", onKeyUp);
-
+    const clock = new THREE.Clock();
     const animate = () => {
       requestAnimationFrame(animate);
-    
-      if (controls.isLocked) {
-        const speed = 0.1; // Movement speed
-        const verticalSpeed = 1.0; // Vertical movement speed; adjust as needed
-        const forwardDirection = new THREE.Vector3();
-        const rightDirection = new THREE.Vector3();
-        const upDirection = camera.position.clone().normalize(); // Direction from the camera towards the center of the planet
-    
-        // Get the forward direction relative to the camera's current rotation
-        camera.getWorldDirection(forwardDirection);
-        forwardDirection.normalize();
-        rightDirection.crossVectors(camera.up, forwardDirection).normalize();
-    
-        // Project the movement directions onto the tangent plane at the camera's position
-        const tangentForward = forwardDirection.sub(upDirection.clone().multiplyScalar(forwardDirection.dot(upDirection)));
-        const tangentRight = rightDirection.sub(upDirection.clone().multiplyScalar(rightDirection.dot(upDirection)));
-    
-        if (moveState.forward) {
-          camera.position.addScaledVector(tangentForward, speed);
-        }
-        if (moveState.backward) {
-          camera.position.addScaledVector(tangentForward, -speed);
-        }
-        if (moveState.left) {
-          camera.position.addScaledVector(tangentRight, speed);
-        }
-        if (moveState.right) {
-          camera.position.addScaledVector(tangentRight, -speed);
-        }
-    
-        if (moveState.up) {
-          camera.position.setLength(camera.position.length() + verticalSpeed);
-        } else if (moveState.down) {
-          camera.position.setLength(camera.position.length() - verticalSpeed);
-        }
-    
-        // Prevent the camera from going below the surface
-        if (camera.position.length() < WORLD_SIZE + 1) {
-          camera.position.setLength(WORLD_SIZE + 1);
-        }
-      }
-    
+
+      const delta = clock.getDelta();
+      controls.update(delta);
+
       renderer.render(world, camera);
     };
-    
 
     animate();
 
@@ -158,11 +82,12 @@ const ThreeCanvas = () => {
 
     return () => {
       window.removeEventListener("resize", handleResize);
-      document.removeEventListener("keydown", onKeyDown);
-      document.removeEventListener("keyup", onKeyUp);
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('pointerlockchange', handlePointerLockChange);
       if (canvasRef.current) {
         canvasRef.current.removeChild(renderer.domElement);
       }
+      controls.dispose();
     };
   }, []);
 
